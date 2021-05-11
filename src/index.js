de812dd8f96aa88657d82e51f420461e21da0645
David Manor <david@dolphextechnologies.com>
2021-05-11 02:33:34 +0100

    [add] baseweb global providers to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BaseProvider>
+  </StyletronProvider>,
+  document.getElementById("root")
 );
-
