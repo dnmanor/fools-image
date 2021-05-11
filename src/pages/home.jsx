733a9b03fac5d60adfc8850a6db9af15f25c58f1
David Manor <david@dolphextechnologies.com>
2021-05-11 03:46:40 +0100

    [add] search to homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Menu from '../components/header/header'
+import SearchInput from '../components/search/search'
 const Home = () => {
     return (
         <div>
             <Menu />
+            <SearchInput />
         </div>
     )
 }
