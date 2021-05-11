34f98f0bc826b3473f38e62fd47db24c2d081a75
David Manor <david@dolphextechnologies.com>
2021-05-11 03:17:34 +0100

    [add] home page

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Menu from '../components/header/header'
+const Home = () => {
+    return (
+        <div>
+            <Menu />
+        </div>
+    )
+}
+
+export default Home
