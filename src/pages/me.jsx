1a81c0b802f7c40d783200c77162a6c92210940f
David Manor <david@dolphextechnologies.com>
2021-05-12 16:02:36 +0100

    add who page

---
+++ src/pages/me.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Display1, Display3 } from "baseui/typography";
+import { StyledLink } from "baseui/link";
+import MyPic from '../assets/myp.jpeg'
+
+const Me = () => {
+  return (
+    <div style={{padding: '50px', alignContent: 'center', textAlign: 'center'}}>
+        <img src={MyPic} alt="my-pic" style={{height: '40vh'}}/>
+      <Display1>🙄Yup, this dude. Lmao,</Display1>
+      <Display3>
+        I thought of this in the shower a night before April Fool's Day but here
+        i am building it in May. Talk of Quick action!
+        Checkout my {" "} 
+        <StyledLink href="https://github.com/dnmanor">
+      Github
+    </StyledLink> 
+      </Display3>
+    </div>
+  );
+};
+
+export default Me;
