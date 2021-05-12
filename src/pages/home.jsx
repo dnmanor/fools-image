75849e741063ab2017d1c5ef65e438a28206aea7
David Manor <david@dolphextechnologies.com>
2021-05-12 05:29:56 +0100

    fix card list rendering bug

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,17 +4,26 @@ import { Search } from "baseui/icon";
 import { getAntonyms } from "../utils/words";
 import { searchImages } from "../utils/api";
 import { Input } from "baseui/input";
+import ImageCard from "../components/image-card/imagecard";
+import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 
 const Home = () => {
   const [keyword, setKeyword] = React.useState("");
-  //   const [pics, setPics] = useState([]);
+  const [pics, setPics] = useState([]);
 
+  const itemProps = {
+    height: "scale1000",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
   const handleClick = async () => {
     const response = await getAntonyms(keyword);
     const searchTerm = response[0];
     // console.log(searchTerm);
     const images = await searchImages(searchTerm);
     // console.log(images);
+    setPics(images);
   };
 
   return (
@@ -29,12 +38,17 @@ const Home = () => {
           clearable
         />
       </React.Fragment>
-
-      {/* <div className="cards-list">
-        {pics.map((pic) => {
-          <ImageCard Image={pic.urls.full} />;
-        })}
-      </div> */}
+      <FlexGrid
+        flexGridColumnCount={[1, 2, 4]}
+        flexGridColumnGap="scale800"
+        flexGridRowGap="scale800"
+      >
+        {pics.map((pic) => (
+          <FlexGridItem>
+            <ImageCard Image={pic.urls.full} name={pic.description}/>;
+          </FlexGridItem>
+        ))}
+      </FlexGrid>
     </div>
   );
 };
