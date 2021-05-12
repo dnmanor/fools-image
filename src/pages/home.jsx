938382e83f8a8ccdde52e81efc4b677c46a61459
David Manor <david@dolphextechnologies.com>
2021-05-12 03:18:11 +0100

    add search, handle keywords and handle submit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import Menu from '../components/header/header'
-import SearchInput from '../components/search/search'
-const Home = () => {
-    return (
-        <div>
-            <Menu />
-            <SearchInput />
-        </div>
-    )
-}
+import React, { useState } from "react";
+import Menu from "../components/header/header";
+import { Search } from "baseui/icon";
+import { getAntonyms } from "../utils/words";
+import { searchImages } from "../utils/api";
+import { Input } from "baseui/input";
 
-export default Home
+const Home = () => {
+  const [keyword, setKeyword] = React.useState("");
+  //   const [pics, setPics] = useState([]);
+
+  const handleClick = async () => {
+    
+    const response = await getAntonyms(keyword);
+    const searchTerm = response[0];
+    console.log(searchTerm);
+    await searchImages(searchTerm);
+  };
+
+  return (
+    <div>
+      <Menu />
+      <React.Fragment>
+        <Input
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Search a picture"
+          endEnhancer={<Search size="18px" onClick={handleClick}/>}
+          clearable
+        />
+      </React.Fragment>
+
+      {/* <div className="cards-list">
+        {pics.map((pic) => {
+          <ImageCard Image={pic.urls.full} />;
+        })}
+      </div> */}
+    </div>
+  );
+};
+
+export default Home;
